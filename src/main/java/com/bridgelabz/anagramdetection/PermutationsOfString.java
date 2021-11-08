07b1e9dfb32429880440f0882775eba22f0d93b4
PavaniKempula <pavanikempula1998@gmail.com>
2021-11-08 11:49:22 +0530

    ADDED : Printing the output of iterative method and recursion method

---
+++ src/main/java/com/bridgelabz/anagramdetection/PermutationsOfString.java
@@ -0,0 +1,73 @@
+package com.bridgelabz.anagramdetection;
+
+import java.util.Scanner;
+
+/*******************************************************************
+ * @author Pavani
+ *
+ * In this class we are finding all possible permutation of a string.
+ * Using Iterative and Recursion Method.
+ *******************************************************************/
+
+public class PermutationsOfString {
+    //finding string possible permutation using recursion method.
+    public static void recursion(String question, String answer)
+    {
+        if(question.length() == 0)
+        {
+            System.out.print(answer + "\t");
+            return;
+        }
+
+        for(int i = 0; i < question.length(); i++) {
+            char ch = question.charAt(i);
+            String questionLeftPart = question.substring(0, i);
+            String questionRightPart = question.substring(i + 1);
+            String restOfTheQuestion = questionLeftPart + questionRightPart;
+            recursion(restOfTheQuestion, answer + ch); // calling method recursively.
+        }
+    }
+
+    /**
+     * @param str
+     * Finding permutation using iterative method.
+     */
+    public static void iterative(String str) {
+        int n = str.length();
+        int f = factorial(n);
+
+        for(int i = 0; i < f; i++) {
+            StringBuilder sb = new StringBuilder(str);
+            int temp = i;
+            for(int div = n; div >= 1; div--) {
+                int q = temp / div;
+                int r = temp % div;
+
+                System.out.print(sb.charAt(r));
+                sb.deleteCharAt(r);
+
+                temp = q;
+            }
+            System.out.print("\t");
+        }
+    }
+
+    public static int factorial(int n) {
+        int val = 1;
+        for(int i = 2; i <= n; i++) {
+            val *= i;
+        }
+        return val;
+    }
+
+    public static void main(String[] args) {
+        Scanner scannerObject = new Scanner(System.in);
+        System.out.print("Enter The String : ");
+        String userString = scannerObject.next();
+        System.out.print("Recursion output : \t");
+        recursion(userString,"");
+        System.out.print("\nIterative output : \t");
+        iterative(userString);
+        scannerObject.close();
+    }
+}
